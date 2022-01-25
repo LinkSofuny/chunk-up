b208bfda29677a07b4c0d1cce53b00415d061d53
Link <cyd551646@sina.com>
2022-01-25 17:43:42 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # frag-upload
 一个开箱即用的断点续传工具
+
+
+# todo
+TypeScript重构
+慢启动策略
+重传错误切片配置
+vite
---
+++ src/request.js
@@ -0,0 +1,28 @@
+export default function request({
+    url, 
+    method = "post", 
+    data, 
+    headers = {}, 
+    onProgress = e => e,
+    requestList
+}) {
+    return new Promise(resolve => {
+        const xhr = new XMLHttpRequest();
+        xhr.upload.onprogress = onProgress
+        xhr.open(method, url)
+        Object.keys(headers).forEach(key =>
+            xhr.setRequestHeader(key, headers[key])
+        )
+        xhr.send(data)
+        xhr.onload = e => {
+            if (requestList) {
+                const xhrIndex = requestList.findIndex(item => item === xhr)
+                requestList.splice(xhrIndex, 1)
+            }
+            resolve({
+              data: e.target.response
+            })
+        }
+        requestList?.push(xhr)
+    })
+}
